d91ecfeb4c4dda8729d54e7c9675221f4b9f9cb4
Ognjen Blazic <ognjen.b@onthegosystems.com>
2023-07-23 15:26:18 +0200

    api call from client app - example

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,12 @@
-import Image from 'next/image'
-
 export default function Home() {
+  let text = "";
+
+  fetch('http://127.0.0.1:8000/forewords')
+    .then(results => results.json())
+    .then(data => {
+      console.log(data[0].text)
+    });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
