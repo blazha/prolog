b377ac691ced84a0aaddca9c06b8d60a6472d750
Ognjen Blazic <ognjen.b@onthegosystems.com>
2023-07-31 22:35:13 +0200

    error handling

--- api/main.py
+++ api/main.py
@@ -1,10 +1,12 @@
+import json
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
 import crud, models, schemas
 from database import SessionLocal, engine 
 
-import pdb
+from starlette.responses import Response
+from starlette.status import HTTP_200_OK
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -43,4 +45,8 @@ def read_forewords(skip: int = 0, limit: int = 100, db: Session = Depends(get_db
 
 @app.get("/zitije/{month}/{day}", response_model=schemas.Zitije)
 def get_zitije(month: int, day: int, db: Session = Depends(get_db)):
-    return crud.get_zitije(db, month=month, day=day)
+    zitije =  crud.get_zitije(db, month=month, day=day)
+    if zitije is None:
+        return Response(status_code=HTTP_200_OK, content=json.dumps({}))
+    else:
+        return zitije
